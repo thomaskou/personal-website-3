30a5f4427efa8435aad8b66210e95d6cb48badef
Thomas Kou <thomaskou00@gmail.com>
2022-01-02 04:37:15 -0800

    Add Google Analytics via react-ga

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+
+ReactGA.initialize('UA-145825675-1');
+ReactGA.pageview('/');
 
 ReactDOM.render(
 	<React.StrictMode>
